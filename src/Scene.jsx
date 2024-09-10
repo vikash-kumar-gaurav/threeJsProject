b24186b8d5067f55c3f2ca8ce078d6a1997ba7f0
Vikash kumar <vikashkumargauravdss@gmail.com>
2024-09-10 14:33:26 +0530

    updated

---
+++ src/Scene.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from 'react'
+import * as THREE from 'three'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useTexture } from '@react-three/drei'
+
+const Scene = () => {
+
+    let tex = useTexture('img.png');
+    let meshrotate = useRef(null);
+    useFrame((state, delta)=>{
+        meshrotate.current.rotation.y += delta;
+        
+    });
+  return (
+    <><mesh ref={meshrotate} >
+        <cylinderGeometry args={[1, 1, 1, 60, 60, true]}/> 
+        <meshStandardMaterial transparent map={tex} side={THREE.DoubleSide} />
+      </mesh>
+      </>
+  )
+}
+
+export default Scene
